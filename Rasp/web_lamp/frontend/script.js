9433d08d38df53e2609ce80323369ef0e4c7b41a
Nguyen Duc Chung <102210030@sv1.dut.udn.vn>
2024-05-30 00:14:42 +0700

    nice

--- Rasp/web_lamp/frontend/script.js
+++ Rasp/web_lamp/frontend/script.js
@@ -1,23 +1,20 @@
-let socket = new WebSocket('ws://192.168.1.9:6789')
+let socket = new WebSocket('ws://127.0.0.1:6789')
 let red = document.getElementById('red')
 let green = document.getElementById('green')
 let blue = document.getElementById('blue')
-  
+
 socket.onmessage = (event) => {
-  message = JSON.parse(event.data)
-  let button = message.button
-  if (button) {
-    if (button == 3) {
-    } else if (button == 2) {
-    } else if (button == 1) {
+    message = JSON.parse(event.data)
+    let button = message.button
+    if (button) {
+        document.getElementById('btn').innerText = 'button ' + button
+    } else {
+        red.innerText = message.red
+        green.innerText = message.green
+        blue.innerText = message.blue
     }
-  } else {
-    red.innerText = message.red
-    green.innerText = message.green
-    blue.innerText = message.blue
-  }
 }
 
 function toggle(color) {
-  socket.send(color)
+    socket.send(color)
 }
